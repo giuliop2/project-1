ee355b5bb1bc2bd40c0fdb8de0ecd9bf5ffd1c1e
Giulio Pozzuoli <giuliop@uoregon.edu>
2020-04-09 14:34:33 -0700

    add ArrMax to functions.js

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -8,3 +8,18 @@ let arrayAvg = (arr) =>{
 };
 console.log(arrayAvg([6,2,4]));
 console.log(arrayAvg([2,3,5,7,9]));
+
+let arrMax = (arr) => {
+
+};
+
+let arrMax = (arr);
+let largest = 0;
+
+let arrMax = (arr)=> {
+  let largest = 0;
+  if (largest < arr) 
+  largest = arr;
+};
+
+console.log(arrMax([2,3,5,7,9]));
